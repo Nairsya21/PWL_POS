aaae6f0cd1bdd1202f81d20b34681c92c45d6427
Nairsya21 <nairsyasyauqighasila@gmail.com>
2024-09-23 10:24:07 +0700

    praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,15 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // $data = [
-        //     'level_id'=> 2,
-        //     'username'=>'manager_tiga',
-        //     'nama'=>'Manager 3',
-        //     'password'=> Hash::make('12345'),
-        // ];
-        // UserModel::create($data); 
-
-        $userCount = UserModel::where('level_id', 2)->count();
-        // dd($user);
-        return view('user', ['data' =>$userCount]);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id'=> 2
+            ],
+        );
+        $user->save();
+        return view('user', ['data' =>$user]);
     }
 }
