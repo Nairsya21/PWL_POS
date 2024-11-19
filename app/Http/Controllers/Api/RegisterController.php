463879b1b117ea87f95dc15d2747914ded93961f
Nairsya21 <nairsyasyauqighasila@gmail.com>
2024-11-19 12:12:12 +0700

    jobsheet 11 praktikum 1

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -17,16 +17,26 @@ class RegisterController extends Controller
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
             'level_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
+        // Proses upload gambar
+        // if ($request->hasFile('image')) {
+        //     $image = $request->file('image');
+        //     $imagePath = $image->store('posts', 'public'); // Mengunggah gambar ke storage/app/public/posts
+        // } else {
+        //     $imagePath = null; // Jika tidak ada gambar yang diunggah
+        // }
 
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            // 'image' => $request->image,
+            'image' => $request->image->hashName() // Simpan path gambar
         ]);
         if ($user) {
             return response()->json([
@@ -38,5 +48,14 @@ class RegisterController extends Controller
         return response()->json([
             'success' => false,
         ], 409);
+    }
+    public function show()
+    {
+        $users = UserModel::all(); // Retrieve all users from the m_user table
+
+        return response()->json([
+            'success' => true,
+            'users' => $users,
+        ], 200);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -21,17 +21,22 @@ class UserModel extends Authenticatable implements JWTSubject
     {
         return [];
     }
-    use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefiniskan primary key dari tabel yang digunakan
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'avatar', 'created_at', 'updated_at'];
-    protected $hidden = ['password'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'avatar', 'image'];
+    // protected $hidden = ['password'];
     protected $casts =  ['password' => 'hashed'];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image()
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 
     public function profil(): HasOne
